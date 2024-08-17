c97aff5c94c788110eb22e1b88d732d8bf4a88f8
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-08-17 21:34:28 +0900

    Add hard-coded validation of userid and password

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -8,7 +8,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+	
+	private AuthenticationService authenticationService;
+	
+	
 		
+	public LoginController(AuthenticationService authenticationService) {
+		super();
+		this.authenticationService = authenticationService;
+	}
+
 	@RequestMapping(value="login", method = RequestMethod.GET)
 	public String goToLoginPage() {		
 		return "login";
@@ -16,9 +25,22 @@ public class LoginController {
 	
 	@RequestMapping(value="login", method = RequestMethod.POST)
 	public String goToWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap model) {	
-		model.put("name", name);
-		model.put("password", password);
-		return "welcome";
+		
+		if(authenticationService.authenticate(name, password)) {
+			
+			model.put("name", name);
+			model.put("password", password);			
+			
+			return "welcome";
+			
+		} else {
+			
+			model.put("errorMessage", "Invalid Credentials! Please try again!");
+			
+			return "login";
+		}
+		
+		
 	}
 
 }
--- myfirstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+++ myfirstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -11,6 +11,8 @@
 	<body>
 		Welcome to the login page
 		
+		<pre>${errorMessage}</pre>
+		
 		<form method="post">
 			Name: <input type="text" name="name">
 			Password: <input type="password" name="password">
