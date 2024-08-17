f75d1337ac24a7d459eb28367b7346d3f9ab66f3
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-08-17 22:16:32 +0900

    Created the first version of List Todos Page

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
@@ -1,10 +1,15 @@
 package com.in28minutes.springboot.myfirstwebapp.todo;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class TodoService {
-	private static List<Todo> todos;
+	private static List<Todo> todos = new ArrayList<>();	
+	
 	static {
 		todos.add(new Todo(1, "28minutes", "Learn AWS", LocalDate.now().plusYears(1), false));
 		todos.add(new Todo(2, "28minutes", "Learn DevOps", LocalDate.now().plusYears(1), false));
---
+++ myfirstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/listTodos.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>List Todos Page</title>
+</head>
+<body>
+	<body>
+		<div>Welcome to in28minutes</div>
+		<div> Your Todos are : ${todos}</div>
+	</body>
+	
+</body>
+</html>
