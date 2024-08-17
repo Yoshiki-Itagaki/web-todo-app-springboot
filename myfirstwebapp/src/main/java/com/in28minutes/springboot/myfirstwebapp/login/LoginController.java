bcd57f20880d7b3ba8ab55e729e6575f3282aaa8
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-08-17 21:16:30 +0900

    Displayed login credentials in a JSP using Model

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -1,15 +1,24 @@
 package com.in28minutes.springboot.myfirstwebapp.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 		
-	@RequestMapping("login")
-	public String goToLoginPage() {
-		
+	@RequestMapping(value="login", method = RequestMethod.GET)
+	public String goToLoginPage() {		
 		return "login";
+	}
+	
+	@RequestMapping(value="login", method = RequestMethod.POST)
+	public String goToWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap model) {	
+		model.put("name", name);
+		model.put("password", password);
+		return "welcome";
 	}
 
 }
---
+++ myfirstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Welcome Page</title>
+</head>
+<body>
+	<h1>Login JSP</h1>
+	<body>
+		<div>Welcome to in28minutes</div>
+		<div> Your Name: ${name}</div>
+		<div> Your Password: ${password}</div>
+	</body>
+	
+</body>
+</html>
