84fae4ff170c5e9f65534a844d0501dd58ee2fb1
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-08-18 15:22:48 +0900

    Used Command Beans to implement new todo page validations

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/Todo.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/Todo.java
@@ -2,6 +2,8 @@ package com.in28minutes.springboot.myfirstwebapp.todo;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.Size;
+
 public class Todo {
 
 	public Todo(int id, String username, String description, LocalDate targetDate, boolean done) {
@@ -15,6 +17,8 @@ public class Todo {
 
 	private int id;
 	private String username;
+	
+	@Size(min=10, message="Enter at least 10 characters.")
 	private String description;
 	private LocalDate targetDate;
 	private boolean done;
