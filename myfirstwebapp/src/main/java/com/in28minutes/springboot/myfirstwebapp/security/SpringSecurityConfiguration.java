b7b5f28f334939e6a0368c5c648d6c97f4348a5b
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-08-19 16:13:57 +0900

    Added Spring Boot Starter Data JPA and got H2 database ready

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
@@ -4,11 +4,16 @@ import java.util.function.Function;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SpringSecurityConfiguration {
@@ -35,4 +40,19 @@ public class SpringSecurityConfiguration {
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();		
 	}
+	
+	@Bean
+	  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	    http
+	        .csrf(csrf -> csrf
+	            .ignoringRequestMatchers("/h2-console/**")
+	            .disable()
+	        )
+	        .authorizeHttpRequests(auth -> auth
+	            .requestMatchers("/h2-console/**").permitAll()
+	        )
+	        .headers(headers -> headers.frameOptions(FrameOptionsConfig::disable));
+	    return http.build();
+	  }
+	
 }
